93f72afdad59dcd6a5ca5ab7ade6de4d93e1c008
Van Phong <phongckm93@gmail.com>
2022-11-19 21:12:24 +0700

    set up sever  OK

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const express=require('express')
+const express = require("express");
 const app = express();
-app.listen(8080,() => { console.log("server is running"); })
+app.use(express.json());
+app.listen(8080, () => {
+    console.log("server is running");
+});
+app.get("/ping", (req, res) => {
+    res.send("Hello to day");
+});
+// app.get("/api", (request, response) => {
+//     const { id } = request.query;
+//     response.status(200).send([id]);
+// });
+// app.get("/api/:id", (request, response) => {
+//     const { id } = request.params;
+//     const { hoTen, lopHoc } = request.body;
+//     response.status(200).send(hoTen);
+// });
