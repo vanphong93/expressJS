814a72a74478a958bcb3342dbf269bc099022e05
Van Phong <phongckm93@gmail.com>
2022-11-28 20:01:13 +0700

    add EJS

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,16 +1,22 @@
-
 const sequelize = require("../models/index");
-const init_models=require("../models/init-models");
+const init_models = require("../models/init-models");
 const model = init_models(sequelize);
-const {successCode,failCode,errorCode}=require("../config/reponse");
-const getUser =async (req, res) => {
+const { successCode, failCode, errorCode } = require("../config/reponse");
+const getUser = async (req, res) => {
     try {
-        let data=await model.user.findAll();
+        let data = await model.node.findAll();
         // res.status(200).send(data);
-        successCode(res,data,"Lấy dữ liệu thành công")
+        // successCode(res,data,"Lấy dữ liệu thành công")
+        var dataUsers = data;
+        res.render("pages/getUser", {
+            dataUsers: dataUsers,
+            title: "Get all user",
+        });
     } catch (error) {
-        // res.send("error");
-        failCode(res,data,"Failed")
+        res.render("pages/getUser", {
+            dataUsers: [],
+            title: "Failed",
+        });
     }
 };
-module.exports = {getUser}
+module.exports = { getUser };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 app.use(express.json()); //cover json to string
-const cors=require("cors");
-app.use(cors())
-app.listen(8080,() => { console.log("sever is running"); });
-const  rootRoute = require("./routes");
-app.use("/api",rootRoute);
+const cors = require("cors");
+app.use(cors());
+const rootRoute = require("./routes");
+app.use("/api", rootRoute);
+
+// set the view engine to ejs
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function (req, res) {
+    var mascots = [
+        { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+        { name: "Tux", organization: "Linux", birth_year: 1996 },
+        { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
+    ];
+    var tagline =
+        "No programming concept is complete without a cute animal mascot.";
+
+    res.render("pages/index", {
+        mascots: mascots,
+        tagline: tagline,
+    });
+});
+
+// about page
+app.get("/about", function (req, res) {
+    res.render("pages/about");
+});
+
+app.listen(8080);
+console.log("Server is listening on port 8080");
